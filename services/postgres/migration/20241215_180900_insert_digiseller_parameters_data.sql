5466ad86c2e358ab352ff4f3fd81b4fc1a392894
ISylkin <ISylkin@croc.ru>
2024-12-16 11:30:04 +0300

    #feat: add data constants migration

---
+++ services/postgres/migration/20241215_180900_insert_digiseller_parameters_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO digiseller_parameters (id, name)
+VALUES (2574425, 'Tariff');
+
+INSERT INTO digiseller_parameter_values (id, parameter_id, name)
+VALUES (9661554, 2574425, '1 day'),
+       (9661695, 2574425, '3 months'),
+       (9662306, 2574425, '6 months'),
+       (9663144, 2574425, '1 year');
